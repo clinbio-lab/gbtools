a130acf1d25701fdbe8421d7d2d90a9853e7bc6d
Gulya-mur <gulenka10@mail.ru>
2025-05-26 21:46:17 +0300

    add types, update docstrings, fix encode params, remove outdated comments

--- genetic_barcode/core/barcoding.py
+++ genetic_barcode/core/barcoding.py
@@ -1,22 +1,24 @@
 import os
 from typing import Optional
-
-
 from pdf417 import encode, render_image
-
+from PIL import Image
 from genetic_barcode.utils.hashing import run_hash
 
-
-
-def save_png(image, vcf_path , output_filename, output_path) -> None:
+def save_png(
+    image: Image.Image,
+    vcf_path: str,
+    output_filename: Optional[str],
+    output_path: Optional[str]
+) -> None:
     """
     Saves the barcode image as a PNG file. If no output path or filename is provided, 
     they will be derived from the input VCF path.
+    Args:
 
-    :param image: Rendered barcode image to be saved.
-    :param vcf_path: Path to the original VCF file, used to infer default paths.
-    :param output_filename: Optional name for the output image file.
-    :param output_path: Optional directory to save the image in.
+    image: Rendered barcode image to be saved.
+    vcf_path (str): Path to the input VCF file containing genotype data.
+    output_filename (Optional[str]): Custom name for the output PNG file (without extension). If None, it's auto-generated.
+    output_path (Optional[str]): Directory to save the PNG file. If None, saves in the current directory.
     """
     if not output_filename:
         output_filename = os.path.basename(vcf_path) + "_barcode.png"
@@ -34,27 +36,24 @@ def save_png(image, vcf_path , output_filename, output_path) -> None:
 def generate_barcode_image(vcf_path: str, output_filename: Optional[str] = None, output_path: Optional[str] = None) -> object:
 
     """
-    Generates a visual genetic barcode image from a VCF file by first creating a MinHash signature 
-    and then compressing it. The resulting signature is used to generate a PDF417 barcode image.
+    Generates a visual genetic barcode image from a VCF file.
 
-    This function processes the VCF file, encodes the genotypes into an IUPAC string, generates 
-    a MinHash signature, compresses the signature, and creates a barcode image representing 
-    the compressed signature.
+    This function processes the VCF file, encodes the genotype data into a compact string representation,
+    and generates a PDF417 barcode based on this encoded string. The resulting image is saved as a PNG file.
 
-    :param vcf_path: The path to the VCF file that contains genotype data.
-    :param columns: The number of columns in the PDF417 barcode (default is 10).
-    :param encoding: The encoding to be used for the barcode (default is "utf-8").
-    :param security_level: The security level of the barcode, affecting the error correction (default is 2).
-    :return: None.
+    Args:
+        vcf_path (str): Path to the input VCF file containing genotype data.
+        output_filename (Optional[str]): Custom name for the output PNG file (without extension). If None, it's auto-generated.
+        output_path (Optional[str]): Directory to save the PNG file. If None, saves in the current directory.
+
+    Returns:
+        object: The rendered barcode image object.
     """
 
     hash = run_hash(vcf_path)
 
-
-    barcode = encode(hash, columns=8, security_level=3)
+    barcode = encode(hash, columns=6, security_level=3)
     image = render_image(barcode, scale = 1)
-
-
 
     save_png(image = image, vcf_path = vcf_path, output_filename = output_filename, output_path = output_path)
     print(f'Hashvalue: {hash}')
